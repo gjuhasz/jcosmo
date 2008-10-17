e392d8fddb15d046cb7ae032bf8418062d373e14
rafael@enq.ufrgs.br <rafael@enq.ufrgs.br@8ead42b0-9ae3-11dd-8e58-870c635edf7a>
2008-10-17 17:06:22 +0000

    Added lgpl license and copyright notice

--- trunk/cosmosrc/src/br/ufrgs/enq/jcosmo/COSMOSAC.java
+++ trunk/cosmosrc/src/br/ufrgs/enq/jcosmo/COSMOSAC.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2008 Rafael de Pelegrini Soares and Renan Pereira Gerber
+ * 
+ * This file is part of JCosmo.
+ * 
+ * JCosmo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JCosmo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JCosmo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package br.ufrgs.enq.jcosmo;
 
 
