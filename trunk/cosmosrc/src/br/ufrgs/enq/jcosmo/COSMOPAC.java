7a84677069d69b102b3b862f0c2ef8d916a720b0
rpgerber <rpgerber@8ead42b0-9ae3-11dd-8e58-870c635edf7a>
2009-12-01 17:30:22 +0000

    Adjust of the COSMOPAC's parameters.

--- trunk/cosmosrc/src/br/ufrgs/enq/jcosmo/COSMOPAC.java
+++ trunk/cosmosrc/src/br/ufrgs/enq/jcosmo/COSMOPAC.java
@@ -33,16 +33,7 @@ public class COSMOPAC extends COSMOSAC {
 	
 	public COSMOPAC() {
 		// TODO: needs a reparametrization based on experiments (VLE, LLE, etc)!!
-		
-		// teste COST:0.37800887481053136 NP:242
-//		setAEffPrime(6.406312590256911);
-////		setCoord(10.0);
-//		setVnorm(49.08102528394544);
-//		setAnorm(48.8073386838568);
-//		setCHB(24991.230313953296);
-//		setSigmaHB(0.0022658291813892955);
-//		setEpsilon(3.2756158636530275);
-		
+	
 		// teste COST:0.32256382240158127 NP:242
 //		setAEffPrime(6.998695161135107);
 ////		setCoord(10.0);
@@ -52,61 +43,6 @@ public class COSMOPAC extends COSMOSAC {
 //		setSigmaHB(0.006949893900864999);
 //		setEpsilon(10.255836590044746);
 		
-		// teste COST:0.33753272552129526 NP:242
-//		setAEffPrime(7.092291285998105);
-////		setCoord(10.0);
-//		setVnorm(21.501990883549087);
-//		setAnorm(32.252260681169446);
-//		setCHB(42020.95995384622);
-//		setSigmaHB(0.00886888028547375);
-//		setEpsilon(3.8698365749213774);
-//		setSigmaGaussian(true);
-		
-		// teste COST:0.2887708953007761 NP:242
-//		setAEffPrime(6.472620447898134);
-//		setCoord(10.0);
-//		setVnorm(53.159972679322344);
-//		setAnorm(48.679351183766364);
-//		setCHB(22145.117295919925);
-//		setSigmaHB(0.0021335704762264537);
-//		setEpsilon(2.99944948223479);
-				
-		// teste COST:0.2563595580149685 NP:242
-//		setAEffPrime(5.833182929456752);
-////		setCoord(COORD);
-////		setVnorm(VNORM);
-////		setAnorm(ANORM);
-//		setCHB(19049.08512470434);
-//		setSigmaHB(0.0014294669608294712);
-//		setEpsilon(2.812848033405693);
-		
-		// teste COST:0.25779779158503163 NP:242
-//		setAEffPrime(6.01151508164739);
-////		setCoord(COORD);
-////		setVnorm(VNORM);
-////		setAnorm(ANORM);
-//		setCHB(17014.10714339448);
-//		setSigmaHB(0.0014599629489520833);
-////		setEpsilon(EPSILON);
-		
-		// teste COST:0.2568374014591271 NP:242
-////		setAEffPrime(AEFFPRIME);
-////		setCoord(COORD);
-////		setVnorm(VNORM);
-////		setAnorm(ANORM);
-//		setCHB(16967.584194174266);
-//		setSigmaHB(0.0014936105325412134);
-////		setEpsilon(EPSILON);
-		
-		// teste COST:0.4448006278656969 NP:343
-//		setAEffPrime(8.109259259259261);
-//		setCoord(13.555555555555554);
-////		setVnorm(VNORM);
-//		setAnorm(37.114000000000004);
-//		setCHB(131222.66666666666);
-////		setSigmaHB(SIGMAHB);
-//		setEpsilon(6.491948148148148);
-		
 		// teste COST:0.3725789961109445 NP:343 gamma
 //		setAEffPrime(6.5);
 ////		setCoord(COORD);
@@ -115,15 +51,6 @@ public class COSMOPAC extends COSMOSAC {
 //		setCHB(19880.0);
 //		setSigmaHB(0.0027);
 //		setEpsilon(4.7); 
-		
-		// teste COST:0.47677915807855176 NP:343 lngamma
-//		setAEffPrime(6.5);
-////		setCoord(COORD);
-////		setVnorm(VNORM);
-//		setAnorm(34.50);
-//		setCHB(15590.5);
-//		setSigmaHB(0.0027);
-//		setEpsilon(10.25);
 		
 		// teste COST:0.3514230794222891 NP:343 lngamma%
 //		setAEffPrime(6.82037037037037);
@@ -143,15 +70,41 @@ public class COSMOPAC extends COSMOSAC {
 //		setSigmaHB(0.0064);
 //		setEpsilon(6.090955782578806);
 		
-		// optimized parameters COST:0.3455019859714253 NP:343 lngamma%
-		setAEffPrime(7.25);
-		setCoord(10.0);
-		setVnorm(66.69);
-		setAnorm(25.85);
-		setCHB(85580.0);
-		setSigmaHB(0.0064);
-		setEpsilon(4.971);
+// 		// optimized parameters COST:0.3455019859714253 NP:343 lngamma% am1 *
+//		setAEffPrime(7.25);
+//		setCoord(10.0);
+//		setVnorm(66.69);
+//		setAnorm(25.85);
+//		setCHB(85580.0);
+//		setSigmaHB(0.0064);
+//		setEpsilon(4.971);
+
+		// teste RMS=0.5536787472192798
+//		setAEffPrime(6.577619440499027);//1.0496235771209508
+////		setCoord(10.0);
+////		setVnorm(66.69);
+//		setAnorm(27.237869264957105);//3.905968470576764
+//		setCHB(53921.70250431103);//20611.804905777906
+//		setSigmaHB(0.0064);
+//		setEpsilon(7.399713408532802);//3.715212187255123
 		
+		//** Aarticle 
+		setAEffPrime(6.5);
+//		setCoord(10.0);
+//		setVnorm(66.69);
+		setAnorm(27.25);
+		setCHB(53920.0);
+		setSigmaHB(0.0064);
+		setEpsilon(7.395);
+		
+		// teste RMS=0.5536787472192798
+//		setAEffPrime(7.0);
+////		setCoord(10.0);
+////		setVnorm(66.69);
+//		setAnorm(30.61019362439002);//6.766426644846354
+//		setCHB(45064.750662825354);//22997.66254830905
+//		setSigmaHB(0.0064);
+//		setEpsilon(12.233500508453352);//14.634406910081982
 	}
 
 	public void setComponents(COSMOSACCompound comps[]) throws Exception {
