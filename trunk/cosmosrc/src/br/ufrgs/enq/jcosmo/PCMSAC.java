b050ca9d6865ec044a42c67d048ab1e7320531af
rafael@enq.ufrgs.br <rafael@enq.ufrgs.br@8ead42b0-9ae3-11dd-8e58-870c635edf7a>
2010-04-14 17:58:04 +0000

    Minor fix for PCM profiles and also added a PCM-based model.

---
+++ trunk/cosmosrc/src/br/ufrgs/enq/jcosmo/PCMSAC.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2008 Rafael de Pelegrini Soares and Renan Pereira Gerber
+ * 
+ * This file is part of JCosmo.
+ * 
+ * JCosmo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JCosmo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JCosmo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package br.ufrgs.enq.jcosmo;
+
+
+/**
+ * PCM-SAC (PCM/GAMAESS sigma profiles) activity model.
+ *
+ * @author Rafael de Pelegrini Soares
+ * 
+ */
+public class PCMSAC extends COSMOSAC {
+	public String toString(){
+		return "PCM-SAC";
+	}
+
+	public PCMSAC(int numberOfSegments) {
+		super(numberOfSegments);
+	}
+
+	public PCMSAC() {
+	}
+
+	public void setComponents(COSMOSACCompound comps[]) throws Exception {
+		this.ncomps = comps.length;
+
+		this.VCOSMO = new double[ncomps];
+		this.area = new double[ncomps][];
+
+		SigmaProfileGenerator s = new SigmaProfileGenerator(SigmaProfileGenerator.FileType.GAMESS_PCM,
+				SigmaProfileGenerator.RAV, nsegments);
+		for (int i = 0; i < comps.length; i++) {
+			String name = comps[i].name.replace(' ','_');
+			String extension = ".pcm.gout";
+			
+			try {
+				s.parseFile("mopac/" + name + extension);												
+			} catch (Exception e) {
+				s.parseFile("mopac/" + name.replace('-','_') + extension);
+			}
+			
+			comps[i].charge = s.getChargeDensity();
+			this.VCOSMO[i] = comps[i].Vcosmo = s.getVolume();
+			this.area[i] = comps[i].area = s.getSortedArea();
+		}
+		this.T = 300;
+
+		z = new double[ncomps];
+		for (int i = 0; i < ncomps; i++)
+			z[i] = 1.0/ncomps;
+
+		parametersChanged();
+	}
+}
