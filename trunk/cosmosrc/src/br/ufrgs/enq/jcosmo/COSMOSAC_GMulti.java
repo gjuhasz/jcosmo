837cef4de2dfcd1acc62b708bb56f3c828a246cb
rafael@enq.ufrgs.br <rafael@enq.ufrgs.br@8ead42b0-9ae3-11dd-8e58-870c635edf7a>
2010-05-11 02:15:46 +0000

    More model tests.

--- trunk/cosmosrc/src/br/ufrgs/enq/jcosmo/COSMOSAC_GMulti.java
+++ trunk/cosmosrc/src/br/ufrgs/enq/jcosmo/COSMOSAC_GMulti.java
@@ -40,88 +40,115 @@ public class COSMOSAC_GMulti extends COSMOSACMulti {
 	public COSMOSAC_GMulti(){
 		super(51, 3);
 		
-		// Only non-polar elements, COST:0.08499795473312625 NP:32
-//		idac/Alkane-Alkane.csv AARD:0.02157167555584826 NP:3
-//		idac/Alkane-AlkylHalide.csv AARD:0.20398044102438556 NP:5
-//		idac/AlkylHalide-Alkane.csv AARD:0.09639315605035846 NP:13
-//		idac/Aromatic-Alkane.csv AARD:0.013181456097686173 NP:6
-//		idac/CycloAlkane-AlkylHalide.csv AARD:0.06062351088595813 NP:5
-		setBeta(0.6720307269285903);
-		setCHB(0.0);
+		// COST:0.13071578444923296 NP:92
+//		idac/Alkane-Alcohol.csv AARD:0.1457625504704864 NP:9
+//		idac/Alkane-Alkane.csv AARD:0.021306165795884433 NP:3
+//		idac/Alkane-AlkylHalide.csv AARD:0.12951704985191428 NP:5
+//		idac/Alkane-Ketone.csv AARD:0.10124178607952884 NP:18
+//		idac/Alkane-Phenol.csv AARD:0.10800138084182385 NP:22
+//		idac/AlkylHalide-Alkane.csv AARD:0.17793718759396865 NP:13
+//		idac/Aromatic-Alkane.csv AARD:0.17738558436957882 NP:6
+//		idac/CycloAlkane-Alcohol.csv AARD:0.18484509202466623 NP:6
+//		idac/CycloAlkane-AlkylHalide.csv AARD:0.035588755861995794 NP:5
+//		idac/CycloAlkane-Phenol.csv AARD:0.22791833281982857 NP:5
+		setBeta(1.8677387992263745);
+		setFpol(0.58049990);
+		setFpol(1, 0.292276224);
+		setFpol(2, 0.421617194);
 		setSigmaHB(0.01);
 		setSigmaHB2(0.0);
 		setSigmaHB3(1.0);
-		setFpol(0.7508070397398836);
+		setCHB(0);
+		setCHB(2, 113220);
 		setIgnoreSG(false);
 		setCoord(10);
-		setAnorm(79.53);
+		setAnorm(107.16496305);
 		setVnorm(66.69);
 		
-//		idac/Alcohol-Alkane.csv AARD:0.4982266327131054 NP:14
-//		idac/Ketone-Alcohol.csv AARD:0.2816430413805225 NP:34, COST:0.3448131635730743
-		setCHB(0);
-		setSigmaHB(0.004);
-		setCHB(1, 120000);
-		
-//		COST:0.27958305302063197
-//		idac/Alcohol-Water.csv AARD:1.6029666923270154 NP:4
-//		idac/Aldehyde-Water.csv AARD:NaN NP:0
-//		idac/Alkane-Water.csv AARD:0.27997699244638746 NP:14
-//		idac/Alcohol-Alkane.csv AARD:0.355200978079096 NP:4
-//		idac/Alcohol-CycloAlkane.csv AARD:0.14638123370339154 NP:5
-//		idac/Alkane-Alcohol.csv AARD:0.3295395129026112 NP:5
-//		idac/Alkane-Alkane.csv AARD:0.019217495724087444 NP:2
-//		idac/Alkane-AlkylHalide.csv AARD:0.16328428366109754 NP:5
-//		idac/Alkane-Ketone.csv AARD:0.3118924529858303 NP:16
-//		idac/Alkane-Phenol.csv AARD:0.23212603926156566 NP:16
-//		idac/AlkylHalide-Alkane.csv AARD:0.2661814486873221 NP:10
-//		idac/Aromatic-Alkane.csv AARD:0.278165824996176 NP:3
-//		idac/CycloAlkane-Alcohol.csv AARD:0.10813960619617895 NP:3
-//		idac/CycloAlkane-AlkylHalide.csv AARD:0.03038726189890581 NP:5
-//		idac/CycloAlkane-Amine.csv AARD:1.5217135556152046 NP:1
-//		idac/CycloAlkane-Phenol.csv AARD:0.1518539060387431 NP:5
-//		idac/Ketone-Alkane.csv AARD:0.08374733315346959 NP:13
-		setBeta(1.1423500947431098);
-		setFpol(0.7726267939852054);
-		setFpol(1, 0.7977790401526919);
-		setCHB(0.0);
-		setCHB(1, 0.3013797759750783);
-		setSigmaHB(0.0025322357476481724);
+		// COST:0.1678591113051289 NP:131
+//		idac/Alcohol-Alkane.csv AARD:0.3494957698383428 NP:10
+//		idac/Alcohol-CycloAlkane.csv AARD:0.35208720003494387 NP:15
+//		idac/Alkane-Alcohol.csv AARD:0.1439600000593901 NP:9
+//		idac/Alkane-Alkane.csv AARD:0.021291524439811763 NP:3
+//		idac/Alkane-AlkylHalide.csv AARD:0.12670897616856733 NP:5
+//		idac/Alkane-Ketone.csv AARD:0.10665831131060521 NP:18
+//		idac/Alkane-Phenol.csv AARD:0.10531834779726368 NP:22
+//		idac/AlkylHalide-Alkane.csv AARD:0.18794664139966455 NP:13
+//		idac/Aromatic-Alkane.csv AARD:0.20539215138547348 NP:6
+//		idac/CycloAlkane-Alcohol.csv AARD:0.14719374417565048 NP:6
+//		idac/CycloAlkane-AlkylHalide.csv AARD:0.030938558548510752 NP:5
+//		idac/CycloAlkane-Phenol.csv AARD:0.23228294906313335 NP:5
+//		idac/Ketone-Alkane.csv AARD:0.07920339041020255 NP:14
+		setBeta(1.6827599663147546);
+		setFpol(0.6407712814407316);
+		setFpol(1, 0.278454604684);
+		setFpol(2, 0.46127354229949);
+		setSigmaHB(0.01);
 		setSigmaHB2(0.0);
+		setSigmaHB3(1.0);
+		setCHB(0);
+		setCHB(2, 113220);
 		setIgnoreSG(false);
-		setCoord(10.0);
-		setAnorm(79.53);
+		setCoord(10);
+		setAnorm(66196.627);
 		setVnorm(66.69);
 		
-		// HB estimation, COST:0.34553669837717527 NP:24
-//		idac/Alcohol-Water.csv AARD:0.52902010362079 NP:8
-//		idac/Aldehyde-Water.csv AARD:0.12205295553595463 NP:2
-//		idac/Alkane-Water.csv AARD:0.27261715667961706 NP:14
+		// COST:0.21093663696082796 NP:145
+//		idac/Alcohol-Alkane.csv AARD:0.3413052989309958 NP:10
+//		idac/Alcohol-CycloAlkane.csv AARD:0.3036033029873327 NP:15
+//		idac/Alkane-Alcohol.csv AARD:0.20581962679001597 NP:9
+//		idac/Alkane-Alkane.csv AARD:0.0317541110073166 NP:3
+//		idac/Alkane-AlkylHalide.csv AARD:0.14190790532239403 NP:5
+//		idac/Alkane-Ketone.csv AARD:0.11258850685166771 NP:18
+//		idac/Alkane-Phenol.csv AARD:0.11121564605853264 NP:22
+//		idac/AlkylHalide-Alkane.csv AARD:0.1437458698552522 NP:13
+//		idac/Aromatic-Alkane.csv AARD:0.18032107248279508 NP:6
+//		idac/CycloAlkane-Alcohol.csv AARD:0.11871154660128531 NP:6
+//		idac/CycloAlkane-AlkylHalide.csv AARD:0.0286507435446411 NP:5
+//		idac/CycloAlkane-Phenol.csv AARD:0.21159016223902838 NP:5
+//		idac/Ketone-Alcohol.csv AARD:0.6522044711099412 NP:14
+//		idac/Ketone-Alkane.csv AARD:0.10727904180436301 NP:14
+		setBeta(1.675128096644348);
+		setFpol(0.658228278044782);
+		setFpol(1, 0.10625869937826941);
+		setFpol(2, 0.4001722977102460);
+		setSigmaHB(0.01);
+		setSigmaHB2(0.0);
+		setSigmaHB3(1.0);
 		setCHB(0);
-		
-		// nonaqueous+aqueous, COST:0.32677799151994164 NP:143
-//		idac/Alcohol-Water.csv AARD:0.4988574700550016 NP:10
-//		idac/Water.csv AARD:0.2687208633249313 NP:23
-//		idac/Alcohol-Alkane.csv AARD:0.5953974262710329 NP:10
-//		idac/Alcohol-CycloAlkane.csv AARD:0.4402448289876905 NP:10
-//		idac/Alkane-Alcohol.csv AARD:0.6500254618124048 NP:9
-//		idac/Alkane-Alkane.csv AARD:0.019350497916921217 NP:2
-//		idac/Alkane-AlkylHalide.csv AARD:0.28924093440961784 NP:5
-//		idac/Alkane-Ketone.csv AARD:0.14803814371151758 NP:16
-//		idac/Alkane-Phenol.csv AARD:0.18924327367406021 NP:16
-//		idac/AlkylHalide-Alkane.csv AARD:0.43796133972889306 NP:10
-//		idac/Aromatic-Alkane.csv AARD:0.5537557307290863 NP:3
-//		idac/CycloAlkane-Alcohol.csv AARD:0.4139295161647365 NP:6
-//		idac/CycloAlkane-AlkylHalide.csv AARD:0.030019360180914084 NP:5
-//		idac/CycloAlkane-Phenol.csv AARD:0.17048964584829668 NP:5
-//		idac/Ketone-Alkane.csv AARD:0.2265411891359618 NP:13
-		setBeta(0.7871234028286334);
-		setFpol(1.5699046534819647);
-		setFpol(1, 0.42939157969075425);
-		setFpol(2, 0.38311458616327376);
-		setSigmaHB(0.006);
-		setCHB(2, 10067);
+		setCHB(2, 32935.717);
+		setIgnoreSG(false);
+		setCoord(10);
+		setAnorm(66196.627);
+		setVnorm(66.69);
 	}
+	
+	protected void calculeDeltaW_HB(){
+		for(int d=0; d<ndescriptors; ++d){
+			for(int d2=0; d2<ndescriptors; ++d2){
+				for(int m=0; m<nsegments; ++m){
+					for(int n=0; n<nsegments; ++n){
+						int ACC = n, DON = m;
+						if(charge[m]>=charge[n]){
+							ACC = m;
+							DON = n;
+						}
+						// Hydrogen Bond effect:
+						double hb = Math.min(0.0, charge[ACC]*charge[DON]);
+
+						// Klamt, Fluid Phase Equilib. 2000
+						// double cHBT_c = 1.5;
+						double cHBT = 1; // Math.max(0, 1 + cHBT_c * (298.15/T - 1));
+						
+						hb = -Math.pow(Math.abs(hb), 2)*1000;
+
+						deltaW_HB[d][d2][m][n] = Math.sqrt(cHB[d]*cHB[d2])*cHBT* hb;
+					}
+				}
+			}
+		}
+	}
+
 
 	public void setComponents(COSMOSACCompound comps[]) throws Exception {
 		if(compList==null)
@@ -147,8 +174,8 @@ public class COSMOSAC_GMulti extends COSMOSACMulti {
 			
 			String name = comps[i].name.replace(' ','_');
 			String extension = ".gout";
-			String folder = "mopac/";
-//			String folder = "moltest/";
+//			String folder = "mopac/";
+			String folder = "moltest/";
 			
 			try {
 				s.parseFile(folder + name + extension);												
@@ -172,6 +199,7 @@ public class COSMOSAC_GMulti extends COSMOSACMulti {
 
 			// value from Klamt (COSMO-RS refinement)
 			double fcorr = 0.816;
+			fcorr = 0.9;
 
 			double[] area = s.getOriginalArea();
 			double[] area2 = new double[area.length];
@@ -187,8 +215,8 @@ public class COSMOSAC_GMulti extends COSMOSACMulti {
 			// only 2 dimensions
 			comps[i].areaMulti = new double[ndescriptors][];
 			
-			double tLimit = 0.3; // so that METHANE is entirely on the first category
-			double tLimit2 = 1;
+			double tLimit = 1; // so that METHANE is entirely on the first category
+			double tLimit2 = 2;
 			for (int m = 0; m < area.length; m++) {
 				if(sigmaT[m]>tLimit){
 					if(sigmaT[m]<tLimit2)
