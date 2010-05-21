5f08d8180961831653bb59c5711bfaf7bafacd0b
rafael <rafael@enq.ufrgs.br>
2010-05-21 02:42:14 +0000

    Some MOPAC tests.

--- cosmosrc/src/br/ufrgs/enq/jcosmo/idac/IDAC_EstFamilies.java
+++ cosmosrc/src/br/ufrgs/enq/jcosmo/idac/IDAC_EstFamilies.java
@@ -19,11 +19,11 @@ import br.ufrgs.enq.jcosmo.COSMOSAC_GMulti;
 import br.ufrgs.enq.jcosmo.COSMOSAC_GMultiAtom;
 import br.ufrgs.enq.jcosmo.PCMSACMulti;
 
-public class IDAC_EstMultiNonHB implements CostFunction, ObjectiveFunction {
+public class IDAC_EstFamilies implements CostFunction, ObjectiveFunction {
 
 	List<IDACExperimentsMulti> experiments;
 
-	public IDAC_EstMultiNonHB() throws Exception {
+	public IDAC_EstFamilies() throws Exception {
 //		String modelClass = COSMOPACMulti.class.getName();
 		String modelClass = COSMOPACMultiAtom.class.getName();
 //		String modelClass = PCMSACMulti.class.getName();
@@ -33,9 +33,9 @@ public class IDAC_EstMultiNonHB implements CostFunction, ObjectiveFunction {
 		experiments = new ArrayList<IDACExperimentsMulti>();
 
 		// or just the two main groups
-		experiments.add(new IDACExperimentsMulti("idac/nonHB.csv", modelClass));
-//		experiments.add(new IDACExperimentsMulti("idac/nonaqueous.csv", modelClass));
-//		experiments.add(new IDACExperimentsMulti("idac/aqueous.csv", modelClass));
+//		experiments.add(new IDACExperimentsMulti("idac/nonHB.csv", modelClass));
+		experiments.add(new IDACExperimentsMulti("idac/nonaqueous.csv", modelClass));
+		experiments.add(new IDACExperimentsMulti("idac/aqueous.csv", modelClass));
 	}
 
 	public boolean getBounds(double[] xl, double[] xu) {
@@ -59,13 +59,13 @@ public class IDAC_EstMultiNonHB implements CostFunction, ObjectiveFunction {
 		}
 	}
 	public int getNumberOfPars(){
-		return 7;
+		return 6;
 	}
 	public void getCurrent(double [] pars){
 		int i=0;
 		COSMOSACMulti cosmo = (COSMOSACMulti) experiments.get(0).getModels().get(0);
 
-		pars[i++] = cosmo.getBeta(0);
+//		pars[i++] = cosmo.getBeta(0);
 //		pars[i++] = cosmo.getBeta(1);
 //		pars[i++] = cosmo.getBeta(2);
 		pars[i++] = cosmo.getFpol();
@@ -73,16 +73,16 @@ public class IDAC_EstMultiNonHB implements CostFunction, ObjectiveFunction {
 		pars[i++] = cosmo.getFpol(2);
 		pars[i++] = cosmo.getFpol(3);
 //		pars[i++] = cosmo.getCHB(0);
-//		pars[i++] = cosmo.getCHB(1,2);
-//		pars[i++] = cosmo.getCHB(1,3);
+		pars[i++] = cosmo.getCHB(1,2);
+		pars[i++] = cosmo.getCHB(1,3);
 //		pars[i++] = cosmo.getCHB(1,4);
 //		pars[i++] = cosmo.getCHB(2);
 //		pars[i++] = cosmo.getSigmaHB();
 //		pars[i++] = cosmo.getSigmaHB2();
 //		pars[i++] = cosmo.getSigmaHB3();
 //		pars[i++] = cosmo.getCoord();
-		pars[i++] = cosmo.getAnorm();
-		pars[i++] = cosmo.getVnorm();
+//		pars[i++] = cosmo.getAnorm();
+//		pars[i++] = cosmo.getVnorm();
 	}
 
 	public double cost(double[] pars) throws CostException {
@@ -98,7 +98,7 @@ public class IDAC_EstMultiNonHB implements CostFunction, ObjectiveFunction {
 				COSMOSACMulti cosmo = exp.getModels().get(j);
 				
 				int i=0;
-				cosmo.setBeta(pars[i++]);
+//				cosmo.setBeta(pars[i++]);
 //				cosmo.setBeta(1, pars[i++]);
 //				cosmo.setBeta(2, pars[i++]);
 				cosmo.setFpol(pars[i++]);
@@ -106,16 +106,16 @@ public class IDAC_EstMultiNonHB implements CostFunction, ObjectiveFunction {
 				cosmo.setFpol(2, pars[i++]);
 				cosmo.setFpol(3, pars[i++]);
 //				cosmo.setCHB(pars[i++]);
-//				cosmo.setCHB(1,2, pars[i++]);
-//				cosmo.setCHB(1,3, pars[i++]);
+				cosmo.setCHB(1,2, pars[i++]);
+				cosmo.setCHB(1,3, pars[i++]);
 //				cosmo.setCHB(1,4, pars[i++]);
 //				cosmo.setCHB(2, pars[i++]);
 //				cosmo.setSigmaHB(pars[i++]);
 //				cosmo.setSigmaHB2(pars[i++]);
 //				cosmo.setSigmaHB3(pars[i++]);
 //				cosmo.setCoord(pars[i++]);
-				cosmo.setAnorm(pars[i++]);
-				cosmo.setVnorm(pars[i++]);
+//				cosmo.setAnorm(pars[i++]);
+//				cosmo.setVnorm(pars[i++]);
 				
 				// update some internal variables
 				cosmo.parametersChanged();
@@ -153,9 +153,9 @@ public class IDAC_EstMultiNonHB implements CostFunction, ObjectiveFunction {
 	}
 
 	public static void main(String[] args) {
-		IDAC_EstMultiNonHB est;
+		IDAC_EstFamilies est;
 		try {
-			est = new IDAC_EstMultiNonHB();
+			est = new IDAC_EstFamilies();
 		} catch (Exception e1) {
 			e1.printStackTrace();
 			return;
